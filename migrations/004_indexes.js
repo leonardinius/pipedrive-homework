00fefd2e7750a9aff13e66f71b8a6cce827dcf39
l.maslovs <l.maslovs@besk.com>
2017-09-21 00:21:14 +0300

    fix indexes

--- migrations/004_indexes.js
+++ migrations/004_indexes.js
@@ -1,7 +1,7 @@
 exports.up = (pgm) => {
-    pgm.createIndex('nodes', ['name'], {method: 'btree'});
+    pgm.sql('CREATE INDEX lower_case_nodes_name ON nodes USING BTREE (lower(name));');
 };
 
 exports.down = (pgm) => {
-    pgm.dropIndex('nodes', ['name'], {method: 'btree'});
+    pgm.sql('DROP INDEX lower_case_nodes_name;');
 };
